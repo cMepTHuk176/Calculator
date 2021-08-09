8cb0d8428035f4b426cc5d7916a1bd163fb60da6
qwest <qwest@DESKTOP-4LAFJKI>
2021-08-09 12:38:43 +0700

    Добавьте файлы проекта.

---
+++ Calculator/Calculator/AppShell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void MenuItem_Clicked(object sender, System.EventArgs e)
+        {
+            await DisplayAlert("", "Привет Хабр!", "OK");
+        }
+    }
+}
---
+++ Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class SharedResources
+    {
+        public static Color OpButtonBkColor => Color.FromHex("#b0c4de");
+        public static Color OnButtonColor => Color.FromHex("#b0c4de");
+    }
+}
