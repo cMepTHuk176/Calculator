1767b8d9f418c263e20153baddbe92eedbb8135d
tidatida46 <groundl22@yandex.ru>
2021-10-11 18:10:14 +0700

    дизайн появится позже, бланк работы для программиста сергея глухова

---
+++ Calculator/Calculator/TextCleaner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public static class TextCleaner
+    {
+        public static void EntryClean(params Entry[] entries)
+        {
+            foreach (var item in entries)
+                item.Text = default;
+        }
+    }
+}
