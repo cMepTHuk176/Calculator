fb562ecd2864a6c9b266c6fd641787f277bb99ff
Сергей Глухов <vpv2019@mail.ru>
2021-10-11 20:00:02 +0700

    Разность квадратов

--- Calculator/Calculator/Page5.xaml.cs
+++ Calculator/Calculator/Page5.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Threading.Tasks;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +11,25 @@ namespace Calculator
         public Page5()
         {
             InitializeComponent();
+        }
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(ValueA, ValueB);
+        }
+
+        private void Result_Move(object sender, EventArgs e)
+        {
+            double ValueA = double.Parse(ValueA.Text);
+            double ValueB = double.Parse(ValueB.Text);
+
+            resultText.Text = Calc_Move(ValuexA, ValueB).ToString();
+        }
+        private double Calc_Move(double ValueA, double ValueB)
+        {
+            double resultMove;
+            resultMove = ValueA^2 - ValueB^2;
+            return resultMove;
         }
     }
 }
