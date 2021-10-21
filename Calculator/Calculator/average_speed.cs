034669e8e9a873027df57a8ef23c649be9f432f8
tidatida46 <groundl22@yandex.ru>
2021-10-21 17:16:16 +0700

    скорость

--- Calculator/Calculator/average_speed.cs
+++ Calculator/Calculator/average_speed.cs
@@ -6,25 +6,39 @@ using Xamarin.Forms.Xaml;
 namespace Calculator
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class equidistant_motion : ContentPage
+    public partial class average_speed : ContentPage
     {
         public average_speed()
         {
             InitializeComponent();
         }
+
         private void OnClear(object sender, EventArgs e)
         {
             resultText.Text = "0";
-            TextCleaner.EntryClean(x0, Vx, t);
+            TextCleaner.EntryClean(S, t);
+        }
+
+        private void OnIndexChanged(object sender, EventArgs e)
+        {
+            if (TextChecker.EntryCheck(S, t))
+                Result_Cylinder(null, null);
         }
 
         private void Result_averageSpeed(object sender, EventArgs e)
         {
-            double valueS = double.Parse(x0.Text);
-            double valuet = double.Parse(Vx.Text);
+            if (!TextChecker.EntryCheck(S, t))
+            {
+                resultText.Text = Volume_Main.ERROR_TEXT;
+                return;
+            }
+
+            double valueS = double.Parse(S.Text);
+            double valuet = double.Parse(t.Text);
 
             resultText.Text = AverageSpeed(valueS, valuet).ToString();
         }
+
         private double AverageSpeed(double valueS, double valuet)
         {
             double resultMove;
