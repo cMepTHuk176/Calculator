4237702167429445df721b9d94d4b13cf6f62e8f
klyuchnikov2001@inbox.ru <klyuchnikov2001@inbox.ru>
2021-10-21 16:52:37 +0700

    Скопировал код меню из объема фигур

--- Calculator/Calculator/mechanic_main.cs
+++ Calculator/Calculator/mechanic_main.cs
@@ -3,18 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Calculator
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page3 : ContentPage
+    public partial class Volume_Main : ContentPage
     {
-        public Page3()
+        private string selected;
+        internal static readonly string ERROR_TEXT = "ОШИБКА";
+
+        public Volume_Main()
         {
             InitializeComponent();
         }
+
+        async void figurelist_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null)
+            {
+                selected = e.Item.ToString();
+            }
+
+            switch (selected)
+            {
+                case "объем сферы":
+                    await Navigation.PushAsync(new sphere_volume());
+                    break;
+
+                case "объем конуса":
+                    await Navigation.PushAsync(new cone_volume());
+                    break;
+
+                case "объем параллепипеда":
+                    await Navigation.PushAsync(new parallelepiped_volume());
+                    break;
+
+                case "объем квадратной пирамиды":
+                    await Navigation.PushAsync(new square_pyramid_volume());
+                    break;
+
+                case "объем треугольной пирамиды":
+                    await Navigation.PushAsync(new triangular_pyramid_volume());
+                    break;
+
+                case "объем цилиндра":
+                    await Navigation.PushAsync(new cylinder_volume());
+                    break;
+            }
+
+             ((ListView)sender).SelectedItem = null;
+        }
+
+
     }
 }
