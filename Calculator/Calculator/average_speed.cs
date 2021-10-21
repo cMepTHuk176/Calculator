101c3b5bed8025d5aa7c9378991e8cf92a697d04
klyuchnikov2001@inbox.ru <klyuchnikov2001@inbox.ru>
2021-10-21 16:49:28 +0700

    Создал начальный вариант средней скорости(на работоспособность не проверяли), хамл страницы нет

--- Calculator/Calculator/average_speed.cs
+++ Calculator/Calculator/average_speed.cs
@@ -1,20 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Threading.Tasks;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Calculator
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page3 : ContentPage
+    public partial class equidistant_motion : ContentPage
     {
-        public Page3()
+        public average_speed()
         {
             InitializeComponent();
+        }
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(x0, Vx, t);
+        }
+
+        private void Result_averageSpeed(object sender, EventArgs e)
+        {
+            double valueS = double.Parse(x0.Text);
+            double valuet = double.Parse(Vx.Text);
+
+            resultText.Text = AverageSpeed(valueS, valuet).ToString();
+        }
+        private double AverageSpeed(double valueS, double valuet)
+        {
+            double resultMove;
+            resultMove = valueS/valuet;
+            return resultMove;
         }
     }
 }
