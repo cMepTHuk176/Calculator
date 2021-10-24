0bcfa75e2a1ad10b01a5bfce95362c82c45e74e3
Ruslan_Belov <belovruslan15@mail.ru>
2021-10-24 23:08:59 +0700

    1) Реализован интерфейс; 2) Написаны формулы расчетов погрешности, но пока не используются.

---
+++ Calculator/Calculator/Inaccuracy_main.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inaccuracy_main : ContentPage
+    {
+
+        public Inaccuracy_main()
+        {
+            InitializeComponent();
+        }
+
+        private async void InaccuracyList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            string selected = e.Item.ToString() ?? "";
+
+            Page pageToGo = selected switch
+            {
+                "Расчет погрешности по классу точности прибора" => new accuracy_class()
+            };
+
+            await Navigation.PushAsync(pageToGo);
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ Calculator/Calculator/accuracy_class.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class accuracy_class : ContentPage
+    {
+        private readonly string ErrorInacValue = "Неверная погрешность";
+        private readonly string ErrorMeasValue = "Неверное измеренное значение";
+        private readonly string ErrorLimValue = "Неверный предел измерения";
+
+        public accuracy_class()
+        {
+            InitializeComponent();
+            dependencyPicker.SelectedIndex = 0;
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(entryInacValue, entryMeasValue, entryLimValue);
+        }
+
+        private void result_Inaccuracy(object sender, EventArgs e)
+        {
+
+
+        }
+
+    }
+}
