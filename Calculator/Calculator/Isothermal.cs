5364c723dc6689c6b3ea213e16c6310c6b40dc23
Илья Айкин <Aikin9235045396@outlook.com>
2021-10-28 00:55:06 +0700

    написал код для изотермического процесса, на работоспособность не проверял, хамл страницы нет

--- Calculator/Calculator/Isothermal.cs
+++ Calculator/Calculator/Isothermal.cs
@@ -1,72 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Threading.Tasks;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Calculator
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Page1 : ContentPage
+    public partial class Isothermal : ContentPage
     {
-        public int pages = 0;
-        public Page1()
+        public Isothermal()
         {
             InitializeComponent();
         }
-
-        private void Button_Clicked(object sender, System.EventArgs e)
+        private void OnClear(object sender, EventArgs e)
         {
-            string pageXAML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
-                "<ContentPage xmlns=\"http://xamarin.com/schemas/2014/forms\"\n" +
-                "xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\n" +
-                "x:Class=\"Calculator.Page1\">\n" +
-                "Title=\"Main Page\">\n" +
-                "<Label  Text=\"ERR\" FontSize=\"36\" />" +
-                "</ContentPage>";
-            switch (pages)
-            {
-                case 0:
-                    pageXAML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
-                    "<ContentPage xmlns=\"http://xamarin.com/schemas/2014/forms\"\n" +
-                    "xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\n" +
-                    "x:Class=\"Calculator.Page2\">\n" +
-                    "<StackLayout>\n" +
-                    "<Label  Text=\"один\" FontSize=\"24\" />\n" +
-                    "<Button Text = \"Change\" Clicked = \"Button_Clicked\" FontSize = \"24\" />\n" +
-                    "</StackLayout>\n" +
-                    "</ContentPage>";
-                    break;
-                case 1:
-                    pageXAML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
-                "<ContentPage xmlns=\"http://xamarin.com/schemas/2014/forms\"\n" +
-                "xmlns:x=\"http://schemas.microsoft.com/winfx/2009/xaml\"\n" +
-                "x:Class=\"Calculator.Page3\">\n" +
-                "<StackLayout>\n" +
-                "<Label  Text=\"два\" FontSize=\"24\" />\n" +
-                "<Button Text = \"Change\" Clicked = \"Button_Clicked\" FontSize = \"24\" />\n" +
-                "</StackLayout>\n" +
-                "</ContentPage>";
-                    break;
-            }
-            
-            this.LoadFromXaml(pageXAML);
-
-            if (pages != 0)
-                pages = 0;
-            else
-                pages = 1;
-
-
-
-
-
-
+            resultText.Text = "0";
+            TextCleaner.EntryClean(p, v);
         }
 
-    }
+        private void Result_Isothermal(object sender, EventArgs e)
+        {
+            double valuep = double.Parse(p.Text);
+            double valuev = double.Parse(v.Text);
 
+            resultText.Text = Calc_Isothermal(valuep, valuev).ToString();
+        }
+        private double Calc_Isothermal (double valuep, double valuev)
+        {
+            double resultMove;
+            resultMove = valuep*valuev;
+            return resultMove;
+        }
+    }
 }
