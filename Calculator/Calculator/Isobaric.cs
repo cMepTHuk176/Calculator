9e009b98400d35f378d554b436202abc0b0365f5
Илья Айкин <Aikin9235045396@outlook.com>
2021-10-28 00:58:53 +0700

    написал код для изохорного процесса, на работоспособность не проверял, хамл страницы нету

---
+++ Calculator/Calculator/Isobaric.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Isothermal : ContentPage
+    {
+        public Isothermal()
+        {
+            InitializeComponent();
+        }
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(p, v);
+        }
+
+        private void Result_Isothermal(object sender, EventArgs e)
+        {
+            double valuep = double.Parse(p.Text);
+            double valuev = double.Parse(v.Text);
+
+            resultText.Text = Calc_Isothermal(valuep, valuev).ToString();
+        }
+        private double Calc_Isothermal (double valuep, double valuev)
+        {
+            double resultMove;
+            resultMove = valuep*valuev;
+            return resultMove;
+        }
+    }
+}
---
+++ Calculator/Calculator/Isochoric.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Isochoric : ContentPage
+    {
+        public Isochoric()
+        {
+            InitializeComponent();
+        }
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(p, t);
+        }
+
+        private void Result_Isochoric(object sender, EventArgs e)
+        {
+            double valuep = double.Parse(p.Text);
+            double valuet = double.Parse(t.Text);
+
+            resultText.Text = Calc_Isothermal(valuep, valuet).ToString();
+        }
+        private double Calc_Isothermal (double valuep, double valuet)
+        {
+            double resultMove;
+            resultMove = valuep/valuet;
+            return resultMove;
+        }
+    }
+}
