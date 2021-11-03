7683039eeb51bc9d40682c32dfdadb0367dea667
Сергей Глухов <vpv2019@mail.ru>
2021-11-03 21:13:41 +0700

    .

--- Calculator/Calculator/Page5.xaml.cs
+++ Calculator/Calculator/Page5.xaml.cs
@@ -23,12 +23,12 @@ namespace Calculator
             double ValueA = double.Parse(ValueA.Text);
             double ValueB = double.Parse(ValueB.Text);
 
-            resultText.Text = Calc_Move(ValuexA, ValueB).ToString();
+            resultText.Text = Square_difference_m(ValuexA, ValueB).ToString();
         }
-        private double Calc_Move(double ValueA, double ValueB)
+        private double Square_difference_m(double ValueA, double ValueB)
         {
             double resultMove;
-            resultMove = ValueA^2 - ValueB^2;
+            resultMove = (ValueA + ValueB) * (ValueA - ValueB);
             return resultMove;
         }
     }
