6669201535a2e0b7d1de426957e3cdeb7acb2331
Сергей Глухов <vpv2019@mail.ru>
2021-11-03 21:34:21 +0700

    функции

--- Calculator/Calculator/Page5.xaml.cs
+++ Calculator/Calculator/Page5.xaml.cs
@@ -23,7 +23,9 @@ namespace Calculator
             double ValueA = double.Parse(ValueA.Text);
             double ValueB = double.Parse(ValueB.Text);
 
-            resultText.Text = Square_difference_m(ValuexA, ValueB).ToString();
+            resultText.Text = Square_difference_m(ValueA, ValueB).ToString();
+            resultText.Text = Сube_difference_m(ValueA, ValueB).ToString();
+            resultText.Text = Cube_sum_m((ValueA, ValueB).ToString();
         }
         private double Square_difference_m(double ValueA, double ValueB)
         {
@@ -31,5 +33,18 @@ namespace Calculator
             resultMove = (ValueA + ValueB) * (ValueA - ValueB);
             return resultMove;
         }
+        private double Сube_difference_m(double ValueA, double ValueB)
+        {
+            double resultMove1;
+            resultMove1 = (ValueA - ValueB) * (ValueA * ValueA + ValueA * ValueB + ValueB * ValueB);
+            return resultMove1;
+        }
+        private double Cube_sum_m(double ValueA, double ValueB)
+        {
+            double resultMove2;
+            resultMove2 = (ValueA + ValueB) * (ValueA * ValueA - ValueA * ValueB + ValueB * ValueB);
+            return resultMove2;
+        }
+
     }
 }
