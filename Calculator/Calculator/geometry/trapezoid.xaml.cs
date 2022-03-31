0ff5f5898d8b89d44a7ef1c3f610419c55885687
Ruslan "Skeletor" Belov <belovruslan15@mail.ru>
2022-03-31 15:16:38 +0700

    Структурированы многие файлы

---
+++ Calculator/Calculator/geometry/trapezoid.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator.geometry
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class trapezoid : ContentPage
+    {
+        public trapezoid()
+        {
+            InitializeComponent();
+        }
+
+        private void OnClear(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Result()
+        {
+
+        }
+    }
+}
