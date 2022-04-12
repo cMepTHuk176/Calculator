0926591023a6c68967700ffe235a5bd150af4ab9
Voll0 <jaroslav.2001.ru@gmail.com>
2022-04-12 18:29:59 +0700

    Создал дизаайн для квадратных уравнений (будет дополняться)

---
+++ Calculator/Calculator/Equathion.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Equathion : ContentPage
+    {
+        public Equathion()
+        {
+            InitializeComponent();
+
+        }
+        private void OnClear(object sender, EventArgs e)
+        {
+            resultText.Text = "0";
+            TextCleaner.EntryClean(entryText_A);
+            TextCleaner.EntryClean(entryText_B);
+            TextCleaner.EntryClean(entryText_C);
+        }
+    }
+}
